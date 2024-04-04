5c565b7dfda10d441e2a7d1ecb8879ac547c4f6e
ngotwaaron <ngotwaaron@gmail.com>
2024-04-04 10:40:07 +0300

    complete phase 1 tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) =>{e.preventDefault()
+    createToDo(e.target['new-task-description'].value)
+  })
 });
+
+function createToDo(description){
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  let listItems = document.createElement('li');
+  listItems.textContent = description;
+  listItems.appendChild(btn)
+  document.querySelector('#tasks').appendChild(listItems)
+}
+function handleDelete(e){
+ e.target.parentNode.remove()
+}
